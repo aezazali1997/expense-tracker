864ec803a353efcb23aa31486b82917392c5a99a
aezazali1997 <36494368+aezazali1997@users.noreply.github.com>
2020-12-18 18:37:09 +0500

    removed all warnings and finalized the code

--- src/components/CurrentBalance/CurrentBalance.js
+++ src/components/CurrentBalance/CurrentBalance.js
@@ -4,9 +4,9 @@ import { GlobalContext } from "../context/Globalstate";
 const CurrentBalance = () => {
 	const { transactions } = useContext(GlobalContext);
 	let total = 0;
-	for (let i = 0; i < transactions.length; i++) {
-		total += parseInt(transactions[i].transaction);
-	}
+	transactions.forEach((transaction) => {
+		total += parseInt(transaction.transaction);
+	});
 	return (
 		<div>
 			<h4>Current balance</h4>
